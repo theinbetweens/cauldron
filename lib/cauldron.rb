df16a877f284d2a3b55bdef16aa6e3e2409d3b7c
theinbetweens <warrensangster@yahoo.com>
2012-02-11 18:10:29 +0000

    Adding lib directory

---
+++ lib/cauldron.rb
@@ -0,0 +1,5 @@
+require 'rubygems'
+require 'logger'
+
+require 'cauldron/pot'
+require 'cauldron/terminal'
---
+++ lib/cauldron/pot.rb
@@ -0,0 +1,11 @@
+module Cauldron
+
+  class Pot
+    
+    def initialize
+      load_saved_theories
+    end
+    
+  end
+  
+end
