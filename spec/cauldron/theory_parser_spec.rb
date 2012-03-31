a964ae2e12fca9a773f135c161ce80e2700fc3ad
theinbetweens <warrensangster@yahoo.com>
2012-03-31 19:44:22 +0100

    Passing theory parser

--- spec/cauldron/theory_parser_spec.rb
+++ spec/cauldron/theory_parser_spec.rb
@@ -9,16 +9,16 @@ module Cauldron
       let(:data) {%q{
 dependents:
   -
-    if RUNTIME_METHOD.kind_of?(RuntimeMethod)
+    "if RUNTIME_METHOD.kind_of?(RuntimeMethod)
       return true
-    end
+    end"
   -
-    if ARG_1 == OUTPUT
+    "if ARG_1 == OUTPUT
       return true
-    end
+    end"
 action:
   statement: "return x"
-  values
+  values:
     x: ARG_1
   position: RUNTIME_METHOD.first.statement_id
 results:
@@ -27,69 +27,27 @@ results:
     }
     }
       it 'should generate a theory instance from a valid theory file' do 
-        parser = TheoryParser.new
-        f = double(:file) 
-        f.stub(:read) { StringIO.new(data.strip) }
-        File.stub(:open).with('example.yml').and_return(f)      
-        parser.parse('example.yml').should be_an_instance_of(Theory)
+        File.stub(:open).with('example.yml').and_yield(StringIO.new(data.strip))              
+        TheoryParser.new.parse('example.yml').should be_an_instance_of(Theory)
       end
       
       it 'should generate an accurate runtime method' do
         parser = TheoryParser.new
-        f = mock(File)
-        #f.stub(:read) { StringIO.new(data.strip) }
-#t = %q{
-#  dependents:'adfdas'
-#}
-        f.stub(:read) { StringIO.new(t.strip) }
-        f.stub(:load) { StringIO.new(t.strip) }
-        
-        
-        #f.stub(:open).and_yield { StringIO.new(data.strip) }
-        
-        #f.stub(:open).and_yield { 'safasdf a' }
-        
-        #File.stub(:open).with('example.yml').and_yield { 'safasdf a' }
-        
-        puts File.open('/home/wsangster/workspace/cauldron/example_1.yml')
-        puts '======================='
-        #File.stub(:open).with('example.yml').and_return(StringIO.new(data.strip))
-        #f.stub!(:open).and_return(StringIO.new(data.strip))
-        #f.stub!(:open).and_return(StringIO.new(data.strip))
-        #f.stub(:load).and_return(StringIO.new(t.strip))
-        #f.should_receive(:load)
-        #File.stub(:open).with('example.yml').and_return(f)
-              
         File.stub(:open).with('example.yml').and_yield(StringIO.new(data.strip))
-        
-        File.should_receive(:open).with('example.yml')
-        
-        #puts parser.parse('example.yml')
-        #YAML.stub(:load).with('example.yml').and_return(f)
-        parser.parse('example.yml').should == Theory.new(
+
+        TheoryParser.new.parse('example.yml').should == Theory.new(
           [
-            "if RUNTIME_METHOD.kind_of?(RuntimeMethod)
-              return true
-            end",
-            "if ARG_1 == OUTPUT
-              return true
-            end"                        
+%q{
+if RUNTIME_METHOD.kind_of?(RuntimeMethod) return true end
+}.strip,
+%q{
+if ARG_1 == OUTPUT return true end
+}.strip                        
           ],
-          {:statement => 'return x',:values => {:x => 'ARG_1'},:position => 'RUNTIME_METHOD.first.statement_id'},
+          {'statement' => 'return x','values' => {'x' => 'ARG_1'},'position' => 'RUNTIME_METHOD.first.statement_id'},
           ['RUNTIME_METHOD.all_pass(ARG_1)']
         )       
       end
-#       
-      # it 'test' do 
-        # f = mock(File)
-#         
-        # #File.stub!(:open).and_return(StringIO.new(data.strip))
-        # #File.stub(:open).and_return(StringIO.new(data.strip))
-        # File.stub(:open).with('test').and_return(StringIO.new(data.strip))
-        # #f.stub(:open).with('test').and_yield { 'safasdf a' }
-#         
-        # puts File.open('test')
-      # end
       
     end
     
