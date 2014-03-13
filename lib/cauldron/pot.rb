93c5be4cf13a91d881414200f40ac6655e8ed5ef
Warren Sangster <warrensangster@Warrens-MacBook-Air.local>
2014-03-13 07:25:47 +0000

    Removing legacy code

--- lib/cauldron/pot.rb
+++ lib/cauldron/pot.rb
@@ -1,19 +1,6 @@
 module Cauldron
 
   class Pot
-    
-    def generate(cases)
-      if theories.empty?
-        return "There aren't any theories loaded so Cauldron is unable to generate a solution"
-      end
-
-      # Generate a resulting function using the available theories
-      empty_function = Function.new
-      # TODO I need a proper $STDOUT check so I don't need this puts
-      output = empty_function.apply_theory(theories.first,cases.first).write
-      puts output
-      output
-    end
 
     def solve(problems)
 
@@ -40,18 +27,8 @@ module Cauldron
       result
 
     end
-    
-    def load_theory(filepath)
-      theory = TheoryParser.new.parse(filepath)
-      theories << theory
-      return true
-    end
 
-  protected    
-    
-    def theories
-      @theories ||= []
-    end
+  protected
 
     def quote(value)
       if value.kind_of?(String)
--- spec/cauldron/pot_spec.rb
+++ spec/cauldron/pot_spec.rb
@@ -2,21 +2,7 @@ require 'spec_helper'
 
 module Cauldron
   
-  describe 'Pot' do 
-    
-    describe '#load_theory' do
-      
-      it 'should be able to load theory' do
-        pot = Pot.new
-        pot.load_theory(File.join('theories','example_1.yml')).should == true
-      end
-      
-      it "should raise an exception if the theory is missing" do
-        pot = Pot.new
-        lambda {pot.load_theory('not_there.yml')}.should raise_exception(Errno::ENOENT)
-      end
-      
-    end
+  describe 'Pot' do
 
     describe '#solve' do
 
@@ -63,53 +49,6 @@ end
 
       end
 
-    end
-    
-    describe '#generate' do
-      
-      it 'should return a runtime method' do
-        pot = Pot.new
-        pot.load_theory(File.join('theories','example_1.yml'))
-        pot.generate([["sparky","sparky"]]).should == 
-"""
-def extend_function_test_method(var1)
-  return var1
-end                  
-""".strip
-      end
-
-      it 'generates a hard coded value method' do
-        pot = Pot.new
-        pot.load_theory(File.join('theories','example_2.yml'))
-        pot.generate([["sparky","sparky"]]).should == 
-%q{
-def extend_function_test_method(var1)
-  return "sparky"
-end                  
-}.strip        
-      end
-
-      context 'passed two cases' do
-
-        it 'should generate a valid runtime method' do
-          pot = Pot.new
-          pot.load_theory(File.join('theories','example_1.yml'))
-          pot.generate([["sparky","sparky"],["kel","kel"]]).should == 
-"""
-def extend_function_test_method(var1)
-  return var1
-end                  
-""".strip          
-        end
-
-      end
-      
-      it "shouldn't generate a method if hasn't any theories loaded" do
-        pot = Pot.new
-        pot.generate([["sparky","sparky"],['kel','kel']]).should == "There aren't any theories loaded so Cauldron is unable to generate a solution"
-      end
-      
-      
     end
     
   end
